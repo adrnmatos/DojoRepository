3b5a71a4575f5059e5bd593d28db68680ebcfaa2
Adriano Matos <adrnmatos@gmail.com>
2019-07-26 22:15:02 -0400

    cleanUp

--- BankOCRProject/src/br/home/adrnmatos/MainEntry.java
+++ BankOCRProject/src/br/home/adrnmatos/MainEntry.java
@@ -5,16 +5,6 @@ public class MainEntry {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner();
-		
-		String string1 = " _|" + "| |" + "|_|";
-		String string2 = " _ " + "| |" + "|_|";
-		
-		int result = scanner.distance(string1, string2);
-		
-		System.out.println(result);
-		
-		
 	}
 
 }
