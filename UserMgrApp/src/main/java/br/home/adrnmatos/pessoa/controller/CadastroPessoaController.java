4ba6064430fd56ac5fb341018db857a45db5c152
Adriano Matos <adrnmatos@gmail.com>
2019-12-21 11:03:19 -0400

    ongoing work on tutorial part 6

--- UserMgrApp/src/main/java/br/home/adrnmatos/pessoa/controller/CadastroPessoaController.java
+++ UserMgrApp/src/main/java/br/home/adrnmatos/pessoa/controller/CadastroPessoaController.java
@@ -1,5 +1,44 @@
 package br.home.adrnmatos.pessoa.controller;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.home.adrnmatos.model.PessoaModel;
+import br.home.adrnmatos.repository.PessoaRepository;
+import br.home.adrnmatos.usuario.controller.UsuarioController;
+import br.home.adrnmatos.uteis.Uteis;
+
+@Named(value = "cadastroPessoaController")
+@RequestScoped
 public class CadastroPessoaController {
 
+	@Inject
+	private PessoaModel pessoaModel;
+	
+	@Inject
+	private UsuarioController usuarioController;
+	
+	@Inject
+	private PessoaRepository pessoaRepository;
+
+	public PessoaModel getPessoaModel() {
+		return pessoaModel;
+	}
+
+	public void setPessoaModel(PessoaModel pessoaModel) {
+		this.pessoaModel = pessoaModel;
+	}
+	
+	public void salvarNovaPessoa() {
+		
+		pessoaModel.setUsuarioModel(this.usuarioController.getUsuarioSession());
+		
+		// indicate person is created on input screen
+		pessoaModel.setOrigemCadastro("I");
+		
+		pessoaRepository.salvarNovoRegistro(pessoaModel);
+		
+		Uteis.mensagemInfo("Registro criado com sucesso");
+	}
 }
---
+++ UserMgrApp/src/main/java/br/home/adrnmatos/uteis/LocalDateTimeAttributeConverter.java
@@ -0,0 +1,26 @@
+package br.home.adrnmatos.uteis;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.persistence.AttributeConverter;
+
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+	@Override
+	public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+		if(localDateTime != null)
+    		return Timestamp.valueOf(localDateTime);
+		
+		return null;
+	}
+
+	@Override
+	public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+		if(timestamp != null)
+    		return timestamp.toLocalDateTime();
+		
+		return null;
+	}
+
+}
